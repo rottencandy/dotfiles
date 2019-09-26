794e5fa578bc13463f8009c20b4c4fffde410fc2
Mohammed Saud <msaud@redhat.com>
2019-09-26 22:19:32 +0530

    add more fancy stuff

--- config/sxhkd/lock.sh
+++ config/sxhkd/lock.sh
@@ -1,5 +1,16 @@
-#!/usr/bin/bash
-i3lock -e -c 000000
+#!/bin/bash
+
+icon="$HOME/.config/sxhkd/lock.png"
+img="$HOME/.cache/i3lock.png"
+
+scrot $img
+# Pixelate image
+convert $img -scale 10% -scale 1000% $img
+# Blur image
+#convert $img -blur 0x4 500% $img
+convert $img $icon -gravity center -composite $img
+i3lock -e -p default -i $img
+
 sleep 5
 xset dpms force off
 
