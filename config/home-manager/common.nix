4b5765460953a3b16ef7df232dde77abea3d232d
Mohammed Saud <md.saud020@gmail.com>
2023-09-27 22:55:55 +0530

    add temporary hyprland config

--- config/home-manager/common.nix
+++ config/home-manager/common.nix
@@ -1,17 +1,24 @@
 { config, pkgs, ... }:
 
+let
+
+  pkgsUnstable = import <nixpkgs-unstable> {};
+
+in
+
 {
   home.packages = with pkgs; [
     hyprland
     wezterm
-    rio
     emacs
     helix
     neovide
     yazi
+    broot
     btop
     bottom
     jless
+    fx
     gron
     yq-go
     xh
@@ -39,4 +46,40 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  programs.rio = {
+    enable = true;
+    settings = {
+      padding-x = 14;
+      # https://github.com/mbadolato/iTerm2-Color-Schemes/blob/master/rio/Jellybeans.toml
+      background = {
+        opacity = 0.5;
+      };
+      colors = {
+        background = "#040404";
+        foreground = "#feffff";
+        selection-background = "#606060";
+        selection-foreground = "#ffffff";
+        cursor = "#feffff";
+        black = "#040404";
+        red = "#d84a33";
+        green = "#5da602";
+        yellow = "#eebb6e";
+        blue = "#417ab3";
+        magenta = "#e5c499";
+        cyan = "#bdcfe5";
+        white = "#dbded8";
+        light_black = "#685656";
+        light_red = "#d76b42";
+        light_green = "#99b52c";
+        light_yellow = "#ffb670";
+        light_blue = "#97d7ef";
+        light_magenta = "#aa7900";
+        light_cyan = "#bdcfe5";
+        light_white = "#e4d5c7";
+      };
+    };
+  };
 }
+
+# vim: fdm=marker:fdl=0:et:sw=2:
